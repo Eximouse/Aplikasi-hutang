753ac93aa864c43f957faac9fe597ff1097193a5
Eximouse <akuorakuros16@gmail.com>
2025-11-26 11:05:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // --- VARIABEL CACHING ---
-const ASSET_CACHE_NAME = 'finpro-v2-modular';
+const ASSET_CACHE_NAME = 'finpro-v2-modified';
 const ASSETS = [
     './',
     './index.html',
